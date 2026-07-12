4cfc794db7e2c03b8a07b0f87401328890d88390
leondood1298 <120069211+leondood1298@users.noreply.github.com>
2026-07-12 03:18:40 -0400

    Resnapshot Scanner Room results on range changes

---
+++ NitroxPatcher/Patches/Dynamic/MapRoomFunctionality_UpdateScanRangeAndInterval_Patch.cs
@@ -0,0 +1,34 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using HarmonyLib;
+using Nitrox.Model.Helper;
+using NitroxClient.GameLogic;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+public sealed class MapRoomFunctionality_UpdateScanRangeAndInterval_Patch : NitroxPatch, IDynamicPatch, INitroxPatch
+{
+    private delegate void PrefixDelegate(MapRoomFunctionality __instance, out float __state);
+
+    private static readonly MethodInfo TARGET_METHOD = Reflect.Method((MapRoomFunctionality t) => t.UpdateScanRangeAndInterval());
+
+    public static void Prefix(MapRoomFunctionality __instance, out float __state)
+    {
+        __state = __instance.GetScanRange();
+    }
+
+    public static void Postfix(MapRoomFunctionality __instance, float __state)
+    {
+        if (__instance.typeToScan != TechType.None && __instance.GetScanRange() != __state)
+        {
+            Resolve<MapRoomScanResultBroadcaster>().BroadcastSnapshot(__instance);
+        }
+    }
+
+    [GeneratedCode("Nitrox.Analyzers", "1.0.13.0")]
+    public override void Patch(Harmony harmony)
+    {
+        PatchMultiple(harmony, TARGET_METHOD, new PrefixDelegate(Prefix).Method, new Action<MapRoomFunctionality, float>(Postfix).Method);
+    }
+}
